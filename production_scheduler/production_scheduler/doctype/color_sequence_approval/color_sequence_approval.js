887059c929dc5f6ba62e7ae380182f4b1c5784df
dharshan <dharsofficialacc@gmail.com>
2026-03-12 10:02:35 +0530

    fix: include missing python and js files for Color Sequence Approval DocType

---
+++ production_scheduler/production_scheduler/doctype/color_sequence_approval/color_sequence_approval.js
@@ -0,0 +1,5 @@
+frappe.ui.form.on('Color Sequence Approval', {
+	refresh: function(frm) {
+		// Add custom buttons for approval if needed in the future
+	}
+});
---
+++ production_scheduler/production_scheduler/doctype/color_sequence_approval/color_sequence_approval.py
@@ -0,0 +1,5 @@
+import frappe
+from frappe.model.document import Document
+
+class ColorSequenceApproval(Document):
+	pass
