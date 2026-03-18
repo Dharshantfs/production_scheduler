24ee866cbaf07bfa577d42028a723a5a8ebaa28a
dharshan <dharsofficialacc@gmail.com>
2026-03-18 16:36:41 +0530

    Fix Pull Orders dialog missing color items by adding custom_item_planned_date to SELECT

--- production_scheduler/api.py
+++ production_scheduler/api.py
@@ -1195,6 +1195,7 @@ def get_color_chart_data(date=None, start_date=None, end_date=None, plan_name=No
 			SELECT
 				i.name as itemName, i.item_code, i.item_name, i.qty, i.uom, i.unit,
 				i.color, i.custom_quality as quality, i.gsm, i.idx, i.custom_plan_code,
+				i.custom_item_planned_date,
 				{so_item_col} {split_col}
 				p.name as planningSheet, p.party_code as partyCode, p.customer,
 				p.ordered_date, p.dod, p.sales_order as salesOrder,
